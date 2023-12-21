bcacdfc2c54c7560640d049e3c9ea8193e579257
WaymonCoulter <waymonlcoulteriii@gmail.com>
2023-12-21 14:16:54 -0600

    Add indexDie.js as copy of index.js

---
+++ indexDie.js
@@ -0,0 +1,11 @@
+console.log("!index.js")
+
+const now = new Date();
+
+console.log(now.toLocaleString())
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(2) === 1 ? "tails" : "heads")
