0bf48da4826fb227f474bc63848c6fc4d50556e4
WaymonCoulter <waymonlcoulteriii@gmail.com>
2023-12-21 14:11:05 -0600

    Flip a coin

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ console.log("!index.js")
 const now = new Date();
 
 console.log(now.toLocaleString())
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(2) === 1 ? "tails" : "heads")
